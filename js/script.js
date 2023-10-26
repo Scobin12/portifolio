4fa825e27212b1f06e2191531d368059c05b3354
João Vitor Scobin <joao_scobin@hotmail.com>
2023-10-25 22:59:57 -0300

    projetos mobile teste01

--- js/script.js
+++ js/script.js
@@ -108,14 +108,28 @@ function resetText() {
 //slider de imagens projeto
 var swiper = new Swiper(".slide-content", {
   slidesPerView: 2,
-  spaceBetween: 100,
+  spaceBetween: 25,
   loop: true,
+  centerSlide: 'true',
+  fade: 'true',
+  grabCursor: 'true',
   pagination: {
     el: ".swiper-pagination",
     clickable: true,
+    dynamicBullets: true
   },
   navigation: {
     nextEl: ".swiper-button-next",
     prevEl: ".swiper-button-prev",
+  },
+
+  breakpoints:{
+    300: {
+      slidesPerView: 1,
+    },
+    520: {
+      slidesPerView: 2,
+    },
+
   },
 });
